05018ba2793e91b582ae31aea8a774d56a8959c6
Swagat-2004 <beheraswagat9437@gmail.com>
2026-09-12 23:37:35 +0530

    some lines edited

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,5 +2,8 @@ public class WelcomeToJava {
     public static void main(String[] args) {
       System.out.println("Welcome to Java");
       System.out.println("I am in Java Track");
+      System.out.println("Welcome to the Java Track");
+      System.out.println("I already know how to build programming logic.");
+      System.out.println("Now I am learning how to express that logic using Java.");
     }
 }
